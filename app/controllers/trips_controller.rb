c6672aeb6f2a80e3f60fad8a5074efacfea5b29e
Elise Dindault <elise.dindault@gmail.com>
2020-09-01 16:16:49 +0200

    CRD on trips

---
+++ app/controllers/trips_controller.rb
@@ -0,0 +1,42 @@
+class TripsController < ApplicationController
+    # INSTALL PUNDIT before_action :set_trip, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @trips = Trip.all
+    end
+
+    def new
+        @trip = current_user.trips.new
+    end
+
+    def create
+        @trip = current_user.trips.new(trip_params)
+        @trip.user_id = current_user.id
+        if @trip.save!
+            redirect_to trips_path # modifier vers le trip_show une fois créé
+        else
+            render :new
+        end
+    end
+
+    def show
+        @trip = Trip.find(params[:id])
+    end
+
+    def destroy
+        @trip.destroy
+        redirect_to trips_path
+    end
+
+    private
+
+    def trip_params
+        params.require(:trip).permit(:name, :starting_date, :end_date)
+    end
+  
+    # PUNDIT
+    # def set_trip
+    #   @trip = Trip.find(params[:id])
+    #   authorize @trip
+    # end
+end
