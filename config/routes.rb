e33a6a2ef855fa553ff7f02da75e3b87de93b8d1
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-09-04 18:07:03 +0200

    front

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :trips, only: [:new, :create, :edit, :update, :destroy, :show, :index]
+  get 'css-cheatsheets', to: 'pages#css-cheatsheets'
 end
