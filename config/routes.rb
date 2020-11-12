f596c1459ee86a5fe1be3b86fc064e316eaf5418
jeremy barbedienne <jeremybarbedienne@gmail.com>
2020-11-13 00:46:12 +0100

    fix bug in prod

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
   resources :trips, only: [:new, :create, :edit, :update, :destroy, :show, :index]
   get 'css-cheatsheets', to: 'pages#css-cheatsheets'
 end
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
